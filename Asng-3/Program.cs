a0a6645c4712ee4af62caee3764dd1e809e5d77d
Gustavo <gustavo.marcano-valero@mohawkcollege.ca>
2020-11-10 12:49:16 -0500

    COMMIT#2 – Source Code Improvements

--- Asng-3/Program.cs
+++ Asng-3/Program.cs
@@ -1,7 +1,14 @@
-﻿using System;
+﻿/**
+ * Lsit of rules applied for best practicing coding
+ * 
+ * - Do organize namespaces with a clearly defined structure.
+ * - Do use Pascal Casing for class names and method names.
+ * - Do use camel Casing for local variables and method arguments.
+ */
+using System;
 using System.Collections.Generic;
 
-namespace COMP10066_Lab3
+namespace Comp10066.Lab3
 {
     /**
      * Library of statistical functions using Generics for different statistical
@@ -11,39 +18,54 @@ namespace COMP10066_Lab3
      * for sample standard deviation calculations
      *
      * @author Joey Programmer
+     * @modified by Gustavo Marcano, November 10, 2020
      */
 
     public class A3
     {
-        public static double Avg(List<double> x, bool incneg)
+        /// <summary>
+        /// Method Average is in charge of calculating the average of a list of numbers that are double
+        /// </summary>
+        /// <param name="list">Represents the list of double</param>
+        /// <param name="includeNegativeValues">Flag parameter that along with the list parameter
+        /// will condition if a negative element will be added to the calculation or not</param>
+        /// <returns>Method Average returns the average of set of numbers in the list</returns>
+        public static double Average(List<double> list, bool includeNegativeValues)
         {
-            double s = Sum(x, incneg);
-            int c = 0;
-            for (int i = 0; i < x.Count; i++)
+            double sum = Sum(list, includeNegativeValues);
+            int counter = 0;
+            for (int i = 0; i < list.Count; i++)
             {
-                if (incneg || x[i] >= 0)
+                if (includeNegativeValues || list[i] >= 0)
                 {
-                    c++;
+                    counter++;
                 }
             }
-            if (c == 0)
+            if (counter == 0)
             {
                 throw new ArgumentException("no values are > 0");
             }
-            return s / c;
+            return sum / counter;
         }
 
-        public static double Sum(List<double> x, bool incneg)
+        /// <summary>
+        /// Method Sum is in charge of calculating the sum of a list's elements that are double
+        /// </summary>
+        /// <param name="list">Represents the list of double</param>
+        /// <param name="includeNegativeValues">Flag parameter that along with the list parameter
+        /// will condition if a negative element will be added to the calculation or not</param>
+        /// <returns>Method Sum returns the sum of the numbers in the list</returns>
+        public static double Sum(List<double> list, bool includeNegativeValues)
         {
-            if (x.Count < 0)
+            if (list.Count < 0)
             {
-                throw new ArgumentException("x cannot be empty");
+                throw new ArgumentException("list cannot be empty");
             }
 
             double sum = 0.0;
-            foreach (double val in x)
+            foreach (double val in list)
             {
-                if (incneg || val >= 0)
+                if (includeNegativeValues || val >= 0)
                 {
                     sum += val;
                 }
@@ -51,6 +73,11 @@ namespace COMP10066_Lab3
             return sum;
         }
 
+        /// <summary>
+        /// Method Median is in charge of calculating median of a list's elements that are double
+        /// </summary>
+        /// <param name="data">Represents the list of double</param>
+        /// <returns>Method Median returns the median of the set of numbers in the list</returns>
         public static double Median(List<double> data)
         {
             if (data.Count == 0)
@@ -67,7 +94,13 @@ namespace COMP10066_Lab3
             return median;
         }
 
-        public static double SD(List<double> data)
+        /// <summary>
+        /// Method StandardDeviation is in charge of calculating the standard deviation of a 
+        /// list's elements that are double
+        /// </summary>
+        /// <param name="data">Represents the list of double</param>
+        /// <returns>Method StandardDeviation returns the median of the set of numbers in the list</returns>
+        public static double StandardDeviation(List<double> data)
         {
             if (data.Count <= 1)
             {
@@ -75,30 +108,30 @@ namespace COMP10066_Lab3
             }
 
             int n = data.Count;
-            double s = 0;
-            double a = Avg(data, true);
+            double sumOfSquares = 0;
+            double average = Average(data, true);
 
             for (int i = 0; i < n; i++)
             {
-                double v = data[i];
-                s += Math.Pow(v - a, 2);
+                double value = data[i];
+                sumOfSquares += Math.Pow(value - average, 2);
             }
-            double stdev = Math.Sqrt(s / (n - 1));
-            return stdev;
+            double standardDeviation = Math.Sqrt(sumOfSquares / (n - 1));
+            return standardDeviation;
         }
 
         // Simple set of tests that confirm that functions operate correctly
         static void Main(string[] args)
         {
-            List<Double> testDataD = new List<Double> { 2.2, 3.3, 66.2, 17.5, 30.2, 31.1 };
+            List<Double> testDataD = new List<Double> { 3.2, -4.3, 62.2, 57.5, -60.2, 11.1 };
 
-            Console.WriteLine("The sum of the array = {0}", Sum(testDataD, true));
+            Console.WriteLine("The sum of the array = {0}", Sum(testDataD, false));
 
-            Console.WriteLine("The average of the array = {0}", Avg(testDataD, true));
+            Console.WriteLine("The average of the array = {0}", Average(testDataD, false));
 
             Console.WriteLine("The median value of the Double data set = {0}", Median(testDataD));
 
-            Console.WriteLine("The sample standard deviation of the Double test set = {0}\n", SD(testDataD));
+            Console.WriteLine("The sample standard deviation of the Double test set = {0}\n", StandardDeviation(testDataD));
         }
     }
 }
